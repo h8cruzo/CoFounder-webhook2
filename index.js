c7d351dd8c393c33eb9717a377d105cf979eadcc
h8cruzo <alihosman375@gmail.com>
2025-07-19 02:54:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// When Lovable sends a response here
 app.post('/webhook', async (req, res) => {
-  console.log('Received webhook from Lovable:', req.body);
+  console.log('Received response from Lovable:', req.body);
 
   try {
-    const response = await fetch('https://hook.us2.make.com/50o6wnkj8a3gikoqsr42odvsa5o4ntu4', {
+    // Forward to Make's Webhook
+    await fetch('https://hook.us2.make.com/50o6wnkj8a3gikoqsr42odvsa5o4ntu4', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body),
     });
 
-    const data = await response.text();
-    console.log('Response from Webhook.site:', data);
-    res.status(200).send('Forwarded to Webhook.site!');
+    res.status(200).send('Forwarded to Make!');
   } catch (err) {
     console.error('Error:', err);
     res.status(500).send('Something went wrong');
