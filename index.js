ad4a13b6405f8834d49af3ad9d6a9dd897cea36e
h8cruzo <alihosman375@gmail.com>
2025-07-19 02:31:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch'); // not axios
+const fetch = require('node-fetch');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -9,22 +9,24 @@ app.post('/webhook', async (req, res) => {
   console.log('Received webhook from Lovable:', req.body);
   
   try {
-   const response = await fetch('https://hook.us2.make.com/50o6wnkj8a3gikoqsr42odvsa5o4ntu4', 
-
-Copy to clipboard
-', {
+    const response = await fetch('https://webhook.site/2781be55-118f-4362-99b8-7a6d97ae50cb', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body),
     });
 
     const data = await response.text();
-    console.log('Response from Make:', data);
-    res.status(200).send('Forwarded to Make!');
+    console.log('Response from Webhook.site:', data);
+    res.status(200).send('Forwarded to Webhook.site!');
   } catch (err) {
     console.error('Error:', err);
     res.status(500).send('Something went wrong');
   }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 const PORT = process.env.PORT || 3000;
